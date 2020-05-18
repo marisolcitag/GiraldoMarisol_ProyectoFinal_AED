7e5af4463e3265d12b5dbfd5016df9e51aef1575
marisolcitag <35766585+marisolcitag@users.noreply.github.com>
2020-05-18 00:46:31 -0400

    Add FinalProjectAED

---
+++ Entrega Final/EntregaFinal_ProyectoAED_NetworkGraph/Test/Tests.java
@@ -0,0 +1,38 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import Model.Principal;
+
+public class Tests {
+	
+	
+	private Principal principal;
+	
+	public Tests() {
+		principal = new Principal(5);
+	}
+	
+	public void initGraph1() {
+		principal = new Principal(5);
+	}
+	
+	public void initGraph2() {
+		principal = new Principal(5);
+	}
+	
+	public void initGraph3() {
+		principal = new Principal(5);
+	}
+	
+	
+	@Test
+	public void test() {
+		assertEquals("", "");
+	}
+	
+	
+	
+	
+	
+}
